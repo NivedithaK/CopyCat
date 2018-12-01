56f936c748b8948a858de331761ab94d7ee4d4b3
Niveditha Kani <34191104+NivedithaK@users.noreply.github.com>
2018-12-01 13:43:16 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,22 +1,7 @@
 document.addEventListener('CopyCat', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://youtube.com';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
-
-
+  var button1 = document.getElementById('checkPage'),
+    content = document.getElementById('printtext').value;
+  button1.onclick = function() {
+    content.innerHTML = document.getElementById('text1').value;
+  };
+}
