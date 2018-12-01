0a7aed9d66824c5f11102749ca9e8b3809198cde
Niveditha Kani <34191104+NivedithaK@users.noreply.github.com>
2018-12-01 17:01:46 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
-let button1 = document.getElementById("copyButton");
+var button1 = document.getElementById("copyButton");
 
 button1.onclick = () => {
-  changeText();
+	console.log(changeText());
+	changeText();
+	console.log("hello");
+	//getSelectionText();
 }
 
 
 function changeText() {
         document.getElementById("textToChange").innerHTML = document.getElementById("textToChange").innerHTML + "<br> " + document.getElementById('mainTextField').value;
+      	//document.getElementById("textToChange").innerHTML = getSelectionText();
       }
+
+ function getSelectionText() {
+ 	console.log("hello");
+    var text = window.getSelection();
+    console.log(text);
+    return text;
+}
+
+document.onMouseUp = getSelectionText();
+
+
