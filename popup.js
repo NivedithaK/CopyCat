b67054c54a202979be2799ad1c625b71dfbdabd9
Niveditha Kani <34191104+NivedithaK@users.noreply.github.com>
2018-12-01 15:20:38 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('checkPage', function() {
-  var button1 = document.getElementById('checkPage'),
-    content = document.getElementById('printtext').value;
-  button1.onclick = function() {
-    content.innerHTML = document.getElementById('text1').value;
-  };
-}
