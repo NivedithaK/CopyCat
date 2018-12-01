d317ce66baf2170407d46e005935baf71b9a7679
Niveditha Kani <34191104+NivedithaK@users.noreply.github.com>
2018-12-01 16:03:11 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
+let button1 = document.getElementbyId("copyButton")
+
+button1.onclick = () => alert("Hello");
+  #button1.addEventListener("click", handler); // Thanks!
+
+
 function changeText() {
         document.getElementById("textToChange").innerHTML = document.getElementById("textToChange").innerHTML + "\n " + document.getElementById('mainTextField').value;
       }
