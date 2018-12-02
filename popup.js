a31ac3dbc81126d6dbebfccba4c4b07842c83d70
Niveditha Kani <34191104+NivedithaK@users.noreply.github.com>
2018-12-01 19:11:02 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,25 +1,55 @@
+// BUTTON 1
+
 var button1 = document.getElementById("copyButton");
 
 button1.onclick = () => {
-	console.log(changeText());
 	changeText();
-	console.log("hello");
-	//getSelectionText();
 }
+
 
 
 function changeText() {
-        document.getElementById("textToChange").innerHTML = document.getElementById("textToChange").innerHTML + "<br> " + document.getElementById('mainTextField').value;
-      	//document.getElementById("textToChange").innerHTML = getSelectionText();
+		chrome.storage.sync.get(['value'],function(result){
+
+		})
+
+
+        document.getElementById("textToChange").innerHTML = document.getElementById("mainTextField").value + " <br> " + document.getElementById("textToChange").innerHTML;
+      	var textToSave = document.getElementById("textToChange").innerHTML;
+      	chrome.storage.sync.set({'value': textToSave});
+
+
+
       }
 
- function getSelectionText() {
- 	console.log("hello");
-    var text = window.getSelection();
-    console.log(text);
-    return text;
+// BUTTON 2
+
+
+var button2 = document.getElementById("copyButton2");
+
+button2.onclick = () => {
+	copyToClipboard(document.getElementById("textToChange").innerHTML);
+}  
+
+function copyToClipboard(str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
 }
 
-document.onMouseUp = getSelectionText();
+// BUTTON 3
 
+var button3 = document.getElementById("clearButton");
 
+button3.onclick = () => {
+	clearAll();
+}
+
+function clearAll() {
+
+ 	document.getElementById("textToChange").innerHTML = "";
+
+}
